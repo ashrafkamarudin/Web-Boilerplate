c728f1860722aa3a2dd1efb67fc282b704ecb4b3
ashrafkamarudin <ashrafkamarudin@hotmail.com>
2017-11-03 20:01:38 +0800

    fix required file path

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,6 @@
 <?php
 
 require_once '../config.php';
-require_once 'includes/functions.php';
+require_once '../vendor/libs/functions.php';
 
 redirect(URL . 'admin/auth'); // redirect to authenticate page
